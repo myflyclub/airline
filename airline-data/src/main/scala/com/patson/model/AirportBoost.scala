19a9a294e9a122385d7763b11bf67770c4b50832
Spencer <spnzrr@gmail.com>
2026-03-03 11:09:35 -0500

    Fix merge conflict

--- airline-data/src/main/scala/com/patson/model/AirportBoost.scala
+++ airline-data/src/main/scala/com/patson/model/AirportBoost.scala
@@ -4,7 +4,7 @@ case class AirportBoost(boostType : AirportBoostType.Value, value : Double) //th
 
 object AirportBoostType extends Enumeration {
   type AirportBoostType = Value
-  val POPULATION, INCOME, ELITE, INTERNATIONAL_HUB, ELITE_CHARM, VACATION_HUB, FINANCIAL_HUB = Value
+  val POPULATION, INCOME, ELITE, INTERNATIONAL_HUB, ELITE_CHARM, VACATION_HUB, FINANCIAL_HUB, PRESTIGE_CHARM = Value
   val getLabel = (boostType : AirportBoostType.Value) => boostType match {
     case POPULATION => "Airport Population"
     case INCOME => "Airport Income Level"
@@ -13,5 +13,6 @@ object AirportBoostType extends Enumeration {
     case ELITE_CHARM => "Elite Strength"
     case VACATION_HUB => "Vacation Hub Strength"
     case FINANCIAL_HUB => "Financial Hub Strength"
+    case PRESTIGE_CHARM => "Airline Prestige"
   }
 }
