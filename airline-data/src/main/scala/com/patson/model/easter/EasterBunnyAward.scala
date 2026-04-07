a7e53d1169c940296e61e087cbe925bf8abe8cf9
Spencer <spnzrr@gmail.com>
2026-04-06 23:00:38 -0400

    Easter game: add missing action point reward

--- airline-data/src/main/scala/com/patson/model/easter/EasterBunnyAward.scala
+++ airline-data/src/main/scala/com/patson/model/easter/EasterBunnyAward.scala
@@ -26,7 +26,7 @@ abstract class EasterBunnyAward(info: SantaClausInfo) {
 
 class EasterCashAward(info: SantaClausInfo) extends EasterBunnyAward(info) {
   override val getType: SantaClausAwardType.Value = SantaClausAwardType.CASH
-  val CASH_AMOUNT = 20000000 * difficultyMultiplier
+  val CASH_AMOUNT = 15000000 * difficultyMultiplier
   override def applyAward(): Unit = {
     AirlineSource.saveLedgerEntry(AirlineLedgerEntry(info.airline.id, CycleSource.loadCycle(), LedgerType.PRIZE, CASH_AMOUNT))
   }
@@ -46,40 +46,49 @@ class EasterServiceQualityAward(info: SantaClausInfo) extends EasterBunnyAward(i
 
 class EasterHqLoyaltyAward(info: SantaClausInfo) extends EasterBunnyAward(info) {
   override val getType: SantaClausAwardType.Value = SantaClausAwardType.HQ_LOYALTY
-  val BONUS = 5 * difficultyMultiplier
+  val BONUS = 15 * difficultyMultiplier
   override def applyAward(): Unit = {
     AirlineCache.getAirline(info.airline.id, fullLoad = true).foreach { airline =>
       airline.getHeadQuarter().foreach { hq =>
-        val bonus = AirlineBonus(BonusType.EASTER_BUNNY, AirlineAppeal(loyalty = BONUS), Some(CycleSource.loadCycle() + Period.yearLength))
+        val bonus = AirlineBonus(BonusType.EASTER_BUNNY, AirlineAppeal(loyalty = BONUS), Some(CycleSource.loadCycle() + 5 * Period.yearLength))
         AirportSource.saveAirlineAppealBonus(hq.airport.id, airline.id, bonus)
       }
     }
   }
   override val description: String = {
     val hq = AirlineCache.getAirline(info.airline.id, fullLoad = true).get.getHeadQuarter().get.airport
-    s"The Easter Bunny is hosting an egg hunt at your HQ for ${Period.yearLength} weeks! 🐣 Loyalty bonus +$BONUS at ${hq.displayText}!"
+    s"The Easter Bunny is hosting an egg hunt at your HQ for ${5 * Period.yearLength} weeks! 🐣 Loyalty bonus +$BONUS at ${hq.displayText}!"
   }
 }
 
 class EasterAirportLoyaltyAward(info: SantaClausInfo) extends EasterBunnyAward(info) {
   override val getType: SantaClausAwardType.Value = SantaClausAwardType.AIRPORT_LOYALTY
-  val BONUS = 20 * difficultyMultiplier
+  val BONUS = 30 * difficultyMultiplier
   override def applyAward(): Unit = {
-    val bonus = AirlineBonus(BonusType.EASTER_BUNNY, AirlineAppeal(loyalty = BONUS), Some(CycleSource.loadCycle() + Period.yearLength))
+    val bonus = AirlineBonus(BonusType.EASTER_BUNNY, AirlineAppeal(loyalty = BONUS), Some(CycleSource.loadCycle() + 5 * Period.yearLength))
     AirportSource.saveAirlineAppealBonus(info.airport.id, info.airline.id, bonus)
   }
-  override val description: String = s"The Easter Bunny stamped your airline logo on every egg being delivered to ${info.airport.displayText}! 🐰 Loyalty bonus +$BONUS at ${info.airport.displayText} for ${Period.yearLength} weeks!"
+  override val description: String = s"The Easter Bunny stamped your airline logo on every egg being delivered to ${info.airport.displayText}! 🐰 Loyalty bonus +$BONUS at ${info.airport.displayText} for ${5 * Period.yearLength} weeks!"
 }
 
 class EasterReputationAward(info: SantaClausInfo) extends EasterBunnyAward(info) {
   override val getType: SantaClausAwardType.Value = SantaClausAwardType.REPUTATION
-  val BONUS = 10 * difficultyMultiplier
+  val BONUS = 25 * difficultyMultiplier
   override def applyAward(): Unit = {
     val newReputation = info.airline.getReputation() + BONUS
     info.airline.setReputation(newReputation)
     AirlineSource.saveAirlineInfo(info.airline)
   }
   override val description: String = s"The Easter Bunny made you the grand marshal of the Easter parade! 🎉 Your airline reputation gets a boost of +$BONUS! (not permanent — the spotlight fades eventually)"
+}
+
+class EasterActionPointAward(info: SantaClausInfo) extends EasterBunnyAward(info) {
+  override val getType: SantaClausAwardType.Value = SantaClausAwardType.ACTION_POINT
+  val actionPoints = 15 * difficultyMultiplier
+  override def applyAward(): Unit = {
+    AirlineSource.adjustAirlineActionPoints(info.airline, actionPoints.toDouble)
+  }
+  override val description: String = s"The Easter Bunny stuffed an action point egg into your basket! 🥚 You receive $actionPoints action points!"
 }
 
 object EasterBunnyAward {
@@ -90,6 +99,7 @@ object EasterBunnyAward {
   def getRewardByType(info : SantaClausInfo, rewardType : SantaClausAwardType.Value) = {
     rewardType match {
       case SantaClausAwardType.CASH => new EasterCashAward(info)
+      case SantaClausAwardType.ACTION_POINT => new EasterActionPointAward(info)
       case SantaClausAwardType.SERVICE_QUALITY => new EasterServiceQualityAward(info)
       case SantaClausAwardType.HQ_LOYALTY => new EasterHqLoyaltyAward(info)
       case SantaClausAwardType.AIRPORT_LOYALTY => new EasterAirportLoyaltyAward(info)
